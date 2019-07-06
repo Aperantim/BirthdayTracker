4614e30a74b44e5dd3af4b754f1d06384b5187e2
Vitaliy Nesterov <liyuop@mail.ru>
2019-07-06 15:19:50 +0300

    Initial Commit

---
+++ BirthdayTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Виталий on 06/07/2019.
+//  Copyright © 2019 Aperantim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
